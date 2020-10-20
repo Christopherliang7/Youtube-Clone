9d6bc83d55aacab4812b45baffb6e69d92013e2a
Nate P <nlpruitt@gmail.com>
2020-10-19 20:08:40 -0400

    Initial Commit for Chris & Nates recast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,47 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import exampleVideoData from "/src/data/exampleVideoData.js";
+import VideoList from "/compiled/src/components/VideoList.js";
+import VideoListEntry from "/compiled/src/components/VideoListEntry.js";
+import VideoPlayer from "/compiled/src/components/VideoPlayer.js";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+    this.updateVideo = this.updateVideo.bind(this);
+  }
+
+  updateVideo(newVideo) {
+    //console.log(event);
+    //console.log(this);
+    this.setState({'currentVideo':newVideo});
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            {/*<div><h5><em>videoPlayer</em> view goes here</h5></div>*/}
+            <VideoPlayer video= {this.state.currentVideo} />
+          </div>
+          <div className="col-md-5">
+            {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
+            <VideoList videos = {this.state.videos} onTitleClick = {this.updateVideo} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,18 @@
-var VideoList = () => (
+import VideoListEntry from '/compiled/src/components/VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
+    {/*<VideoListEntry passedVideo={ props.testVideos[0] } />*/}
+
+    {props.videos.map((video) => {
+      //return <li>{video.kind}</li>
+      return <VideoListEntry video={video} onTitleClick ={props.onTitleClick} />;
+    })};
+    {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div> */}
   </div>
 );
 
